4253620401175d0c71146e74edb858826757d865
thoughtful60 <olgaforeign@gmail.com>
2025-05-18 18:15:28 -0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TariffMeter from './TariffMeter';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TariffMeter />
+  </React.StrictMode>
+);
